5cebec9f0a30add4fbcdbbcd1b5bf83f5c9d530b
Dani Akash <DaniAkash@users.noreply.github.com>
2020-04-05 11:12:08 +0530

    created home component with list of posts

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Post = ({ title, author, content }) => {
   return (
-    <div>
+    <div className={"container"}>
       <h2>{title}</h2>
       <p>{author}</p>
       <p>{content}</p>
     </div>
   );
+};
+
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired
 };
 
 export default Post;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import Post from "../Components/Post";
+import postData from "../mockData/postData";
 
 const Home = () => {
-  return <h2>Home</h2>;
+  const [posts, setPosts] = useState(postData);
+
+  return (
+    <Fragment>
+      {posts.map((post, postIndex) => {
+        return (
+          <Post
+            title={post.title}
+            author={post.author}
+            content={post.content}
+            key={postIndex}
+          />
+        );
+      })}
+    </Fragment>
+  );
 };
 
 export default Home;
