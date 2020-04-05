be7412fbaa5a3152db69d1d8f5766dc3624fa349
Dani Akash <DaniAkash@users.noreply.github.com>
2020-04-05 10:56:06 +0530

    added mock data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./styles.css";
 import Header from "./Components/Header";
+import { Switch, Route } from "react-router-dom";
+import routes from "./routes/routes";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route path={routes.home}>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/mockData/postData.js
@@ -0,0 +1,36 @@
+const postData = [
+  {
+    id: "f498815e-aabb-4c8f-8de4-bac880aa3251",
+    title: "task-force",
+    content:
+      "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    datetime: "1482678406",
+    author: "Sheilah Poltun"
+  },
+  {
+    id: "f498815e-aabb-4c8f-8de4-bac880aa3251",
+    title: "task-force",
+    content:
+      "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    datetime: "1482678406",
+    author: "Sheilah Poltun"
+  },
+  {
+    id: "f498815e-aabb-4c8f-8de4-bac880aa3251",
+    title: "task-force",
+    content:
+      "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    datetime: "1482678406",
+    author: "Sheilah Poltun"
+  },
+  {
+    id: "f498815e-aabb-4c8f-8de4-bac880aa3251",
+    title: "task-force",
+    content:
+      "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    datetime: "1482678406",
+    author: "Sheilah Poltun"
+  }
+];
+
+export default postData;
