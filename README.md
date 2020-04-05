bebb4407351e1173acc514c7f610a7278b7e7d0a
Dani Akash <DaniAkash@users.noreply.github.com>
2020-04-05 10:29:37 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# b7-blog-app-react
-Created with CodeSandbox
---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+const routes = {
+  home: "/home",
+  authors: "/authors",
+  author: "/author/:id",
+  post: "/post/:id",
+  newPost: "/post/add-new",
+  adminLogin: "/login"
+};
+
+export default routes;
