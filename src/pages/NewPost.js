b00a386a7e8dae14df85b20240fdefb5a3128f78
Dani Akash <DaniAkash@users.noreply.github.com>
2020-04-12 10:07:25 +0530

    added submit button

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FormGroup, Label, Input } from "reactstrap";
+import { FormGroup, Label, Input, Form, Button } from "reactstrap";
 
 const NewPost = () => {
   const [authorName, setAuthorName] = useState("");
@@ -10,42 +10,52 @@ const NewPost = () => {
   const onPostTextChange = event => setPostText(event.target.value);
   const onPostTitleChange = event => setPostTitle(event.target.value);
 
+  const submitForm = event => {
+    event.preventDefault();
+    console.log(authorName, postText, postTitle);
+  };
+
   return (
     <div className="container">
-      <FormGroup>
-        <Label for="postTitle">Post Title</Label>
-        <Input
-          type="text"
-          name="post title"
-          id="postTitle"
-          placeholder="Post Title"
-          value={postTitle}
-          onChange={onPostTitleChange}
-        />
-      </FormGroup>
+      <Form>
+        <FormGroup>
+          <Label for="postTitle">Post Title</Label>
+          <Input
+            type="text"
+            name="post title"
+            id="postTitle"
+            placeholder="Post Title"
+            value={postTitle}
+            onChange={onPostTitleChange}
+          />
+        </FormGroup>
 
-      <FormGroup>
-        <Label for="authorName">Author Name</Label>
-        <Input
-          type="text"
-          name="author name"
-          id="authorName"
-          placeholder="Author's Name..."
-          value={authorName}
-          onChange={onAuthorNameChange}
-        />
-      </FormGroup>
+        <FormGroup>
+          <Label for="authorName">Author Name</Label>
+          <Input
+            type="text"
+            name="author name"
+            id="authorName"
+            placeholder="Author's Name..."
+            value={authorName}
+            onChange={onAuthorNameChange}
+          />
+        </FormGroup>
 
-      <FormGroup>
-        <Label for="exampleText">Text Area</Label>
-        <Input
-          value={postText}
-          onChange={onPostTextChange}
-          type="textarea"
-          name="text"
-          id="exampleText"
-        />
-      </FormGroup>
+        <FormGroup>
+          <Label for="exampleText">Text Area</Label>
+          <Input
+            value={postText}
+            onChange={onPostTextChange}
+            type="textarea"
+            name="text"
+            id="exampleText"
+          />
+        </FormGroup>
+        <Button type="submit" onClick={submitForm}>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
